f22ce7ecc8cf914828ece7cd9342a764e1506459
Tekeste Kidanu <tekeste.kidanu@gmail.com>
2017-06-05 22:44:08 -0400

    first blood

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "extends": "airbnb",
+  "plugins": [
+  "react",
+  "jsx-a11y",
+  "import"
+  ],
+  "rules": {
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    'import/extensions': ['off'],
+    'import/no-unresolved': ['off']
+  }
+};
