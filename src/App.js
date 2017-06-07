e4b0bf1a39f6288d382428da9b89f1e0500230ae
tk <tekeste.kidanu@gmail.com>
2017-06-06 23:05:57 -0400

    style and add the Canvas component to App component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Canvas from './components/Canvas';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fontFamily: 'Montserrat',
+      fontSize: '34px',
+      fontWeight: 'SemiBold',
+      textColor: '#9B51E0',
+      backgroundColor: '#F8F3F3',
+    };
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Canvas {...this.state} />
       </div>
     );
   }
