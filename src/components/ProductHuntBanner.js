1ec94531581062c986e03f1455a031bb77725418
Tekeste Kidanu <tekeste.kidanu@gmail.com>
2018-10-02 16:40:04 -0400

    add producthunt banner

--- src/components/ProductHuntBanner.js
+++ src/components/ProductHuntBanner.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default () => (
   <div className="product-hunt">
-    <a href="https://www.producthunt.com/posts/colorgrab">
-      My new project ColorGrab is on Product Hunt! Click here to give me feedback.{' '}
+    <a href="https://www.producthunt.com/posts/hunted-or-nah">
+      My new project Hunted or Nah is on Product Hunt! Click here to give me feedback.{' '}
       <span role="img" aria-label="thank you">
         🙏
       </span>
